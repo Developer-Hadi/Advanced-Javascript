9002db4df8ceb8835e3302cd03b662d6f7624302
Developer-Hadi <hadiuzzamanliton@gmail.com>
2020-03-04 11:12:22 +0600

    Truty-falsy js

---
+++ true-false.js
@@ -0,0 +1,22 @@
+const age = 0;
+const name = "";
+if(name.length){
+    console.log("Condition is true.");
+}
+else{
+    console.log("Condition is false.");
+}
+
+let name2;
+console.log(name2);
+
+// False value;
+// 0
+// ""
+// undefined
+// null
+// NaN
+// false
+
+// Truthy Value;
+// '0', " ", [], "false", 
